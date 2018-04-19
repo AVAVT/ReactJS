e51da57d93481f0f8c45e85488bb47a2040c6317
AVAVT <itsavavt@gmail.com>
2018-04-19 07:37:27 +0700

    Props state

--- Session1/frontend/src/App.js
+++ Session1/frontend/src/App.js
@@ -1,15 +1,31 @@
 import React, { Component } from "react";
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
-// import imagesData from "./data/imagesData.json";
+import axios from "./axios";
 
-import NavBar from './components/NavBar';
+import NavBar from "./components/NavBar";
+import MainContent from "./components/MainContent";
 
 class App extends Component {
+  state = {
+    images: []
+  };
+
+  componentDidMount() {
+    axios
+      .get("/api/images")
+      .then(data => {
+        console.log(data.data);
+        this.setState({ images: data.data });
+      })
+      .catch(err => console.error(err));
+  }
+
   render() {
     return (
       <div className="App">
-        <NavBar />
+        <NavBar onSearchChange={this._onSearchChange}/>
+        <MainContent images={this.state.images} />
       </div>
     );
   }
